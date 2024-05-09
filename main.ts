7fb070d8087b226e5d2d3e41a2b372d1ffbc7047
Ameer Hamza <hamzah95@live.com>
2024-05-10 03:21:12 +0500

    number-guessing-game

---
+++ main.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer"
+
+//generating random number from 1-10
+let randomNumber:number = Math.floor(Math.random() * 10 + 1);
+
+//ask user to guess a number
+let askNumber = await inquirer.prompt({
+    message: "Guess a Number between 1-10",
+    name: "userNum",
+    type: "number"
+})
+
+//conditional statements
+if(askNumber.userNum === randomNumber) {
+    console.log("Congrats! You guessed the right number")
+}
+else{console.log("Try again! Your guessed the wrong number");
+}
